defac2fc90df14bbab137b97a84c6a5933e1effb
Andrew Letskalyuk <andrewletskalyuk@gmail.com>
2024-07-15 11:56:35 +0300

    Mapping

---
+++ Models/InstrumentsClasses/Mapping.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketDataAPI.Models.InstrumentsClasses;
+
+public class Mapping
+{
+    public Guid MappingId { get; set; }
+    public Guid InstrumentId { get; set; }
+    public string Provider { get; set; } = "";
+    public string Symbol { get; set; } = "";
+    public string Exchange { get; set; } = "";
+    public int DefaultOrderSize { get; set; }
+
+    public Instrumenty? Instrumenty { get; set; }
+}
